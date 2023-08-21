84eedf20010252ddd42ec94be357cc5d1e0b27cd
Andressa Yumi Portella <110074166+ayportella@users.noreply.github.com>
2023-08-20 23:53:29 -0300

    Create 01_calculation_with_variables.js

---
+++ 01_calculation_with_variables.js
@@ -0,0 +1,17 @@
+var xampu = 10.50; 
+var sabonete = 3.00; 
+
+var leite = 3.50; 
+var agua = 20; 
+
+var televisao = 1000; 
+var sofa = 1200; 
+var tapete = 1900; 
+
+var gastoBanheiro = 3*xampu + 10*sabonete;
+var gastoCozinha = 1*leite + 2*agua;
+var gastoSala = 1*televisao + 28sofa + 1*tapete
+
+output(gastoBanheiro); 
+output(gastoCozinha); 
+output(gastoSala);
