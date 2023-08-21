8d6807bf9c0b7e5220d531983bfb6d827e528fb5
Andressa Yumi Portella <110074166+ayportella@users.noreply.github.com>
2023-08-21 00:06:46 -0300

    Create 03_calculation_with_variables_addition_or_subtraction.js

---
+++ 03_calculation_with_variables_addition_or_subtraction.js
@@ -0,0 +1,8 @@
+// esse é um código que quaisquer que sejam os calores de a e b, o programa sempre vai imprimir no final o resultado correto (em casos de soma ou subtração entre a e b)
+// op corresponde a 0 ou 1
+
+var a = 10; //qualquer numero inteiro
+var b = 3; // qualquer numero inteiro
+var op = 1; //somente 0 ou 1
+
+output((a-b)*op + (a+b)*(1-op));
