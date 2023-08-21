f59f81ac6b5da0e5bf2e5cb348ac514a03f2e7c9
Andressa Yumi Portella <110074166+ayportella@users.noreply.github.com>
2023-08-21 00:29:57 -0300

    Create 07_input_command_division_operation.js

---
+++ 07_input_command_division_operation.js
@@ -0,0 +1,8 @@
+var inteiro_a = input(); 
+var inteiro_b = input(); 
+
+var resto_divisao = inteiro_a % inteiro_b; 
+var quociente = (inteiro_a - resto_divisao)/ inteiro_b; 
+
+output(quociente); 
+output(resto_divisao);
