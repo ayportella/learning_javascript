bb9adb54f707064c3606ed012ef8aa3dc0ad810a
Andressa Yumi Portella <110074166+ayportella@users.noreply.github.com>
2023-08-21 00:50:51 -0300

    Create 11_if_command_odd_or_even.js

---
+++ 11_if_command_odd_or_even.js
@@ -0,0 +1,11 @@
+//programa que lê número inteiro de entrada e imprime 1 se for par e 0 se for ímpar
+
+var numero = input(); 
+
+if (numero % 2 === 0) {
+output(1); 
+} 
+
+if (numero % 2 === 1) 
+{ output(0); 
+}
